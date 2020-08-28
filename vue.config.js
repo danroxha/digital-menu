aa8d9b1ca73f6c257960c68623abb6340d8d8a09
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-28 13:10:10 -0300

    chore: Alterado o titulo principal da pagina

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   publicPath: '/digital-menu',
   outputDir: '../digital-menu-build',
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Menu Colosso';
+        return args;
+      });
+  },
 }
