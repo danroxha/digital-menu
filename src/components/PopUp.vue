31c6c87c5c982f861c1c076a2d66ab685b16408b
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-28 13:11:17 -0300

    feat: Implementação Incial de  um Pop-up com informações detalhadas dos Cards

---
+++ src/components/PopUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+        <h1>Nome da Pizza</h1>
+    </header>
+    <section>
+    </section>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style>
+</style>
