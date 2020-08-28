493a931c5ef2b06232408ad434c403945696dad6
Daniel Rocha <rochadaniel15@gmail.com>
2020-08-28 01:51:17 -0300

    feat: Implementado os componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id='container'>
+    <Menu></Menu>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Menu from './components/Menu'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Menu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  --_color_0: #ffffff;
+  --_color_1: #f9d125;
+  --_color_2: #3ed979;
+  --_color_3: #9a1511;
+  --_color_4: #edf1f0;
+  --_color_5: #c2c2c2;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+a {
+  text-decoration: none;
+  outline: none;
+}
+body {
+  padding: 0;
+  height: 100vh;
+  background-color: var(--_color_4);
 }
 </style>
