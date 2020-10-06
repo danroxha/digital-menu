64903270cb40786343333d6774acc9386f616868
Daniel Rocha <rochadaniel15@gmail.com>
2020-10-06 11:37:17 -0300

    refactor: Separação das paginas

---
+++ src/pages/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <main id='container'>
+    <Header />
+    <Container>
+      <Cards />
+      <div v-if='visible'>
+        <PopUp />
+      </div>
+    </Container>
+    <Footer />
+  </main>
+</template>
+<script>
+
+import { mapMutations, mapState } from 'vuex'
+import { Card as Cards, Container, Footer, Header, PopUp } from '@/components'
+
+
+export default {
+  
+  components: {
+    Cards, Container, Footer, Header, PopUp
+  },
+
+  computed: {
+    ...mapState('menuItems', ['items']),
+    ...mapState('popUp', ['visible']),
+  },
+  
+  methods: {
+    ...mapMutations('popUp', ['openPopUp']),
+  },
+  
+  mounted: () => {
+    // this.loadData()
+  }
+}
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap');
+
+.title-group{
+  font-family: 'Cedarville Cursive', cursive;
+  font-size: 35pt;
+  text-transform: capitalize;
+  color: var(--_color_3);
+  font-weight: bold;
+  margin-left: 20px;
+}
+
+#container{
+  display: grid;
+  height: 100vh;
+  grid-template-rows: .11fr 1fr .15fr;
+  box-sizing: border-box;
+}
+
+#container section{
+  position: relative;
+  overflow-y: auto;
+  margin: 0 auto;
+}
+
+@media only screen and (min-width: 800px) {
+  #container section{
+    margin: 0 auto;
+    max-width: 100%;
+  }
+}
+
+</style>
