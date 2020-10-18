9d4d9dc4bb562a41f2b368338f6f9ba8d5fed063
Daniel Rocha <rochadaniel15@gmail.com>
2020-10-17 23:26:54 -0300

    feat: Adicionado o 'listener' para obter a posicao do scroll

--- src/components/source/Container.vue
+++ src/components/source/Container.vue
@@ -1,5 +1,14 @@
 <template>
-  <section>
+  <section @scroll="onMoveScroll">
     <slot></slot>
   </section>
 </template>
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  methods: {
+    ...mapMutations("headerVisible", ["onMoveScroll"]),
+  }
+}
+</script>
