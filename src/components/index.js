b24439ecd63228ccb8a2f8cd91e72a47a0f8faf2
Daniel Rocha <rochadaniel15@gmail.com>
2020-10-17 23:22:31 -0300

    feat: Adicionado componente Loading

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as Container } from './source/Container.vue'
 export { default as Footer } from './source/Footer.vue'
 export { default as Header } from './source/Header.vue'
 export { default as PopUp } from './source/PopUp.vue'
-
+export { default as Loading } from './source/Loading.vue'
 
---
+++ src/components/source/Loading.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class='loading'>
+      <h1>Loading Pizza</h1>
+  </div>
+</template>
+<style scoped>
+  .loading {
+    background: green;
+  }
+</style>
