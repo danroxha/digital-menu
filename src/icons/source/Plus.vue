50fc63201cf0d4c6b2e175d9ecd18780bd3bb860
Daniel Rocha <rochadaniel15@gmail.com>
2020-10-18 00:19:17 -0300

    feat: Adicionado icones '+' e '-'

---
+++ src/icons/source/Plus.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg 
+    stroke="currentColor" 
+    fill="currentColor" 
+    stroke-width="0" 
+    t="1551322312294" 
+    viewBox="0 0 1024 1024" 
+    version="1.1" 
+    pId="10297" 
+    height="1em" 
+    width="1em" 
+    xmlns="http://www.w3.org/2000/svg">
+    <defs></defs>
+    <path 
+      d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z" pId="10298"
+    >  
+    </path>
+    <path 
+      d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z" pId="10299"
+    >
+      
+    </path>
+  </svg>
+</template>
