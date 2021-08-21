04ac62a3b54e537704e95a3b3d7d78200839d8ee
Daniel Rocha <rochadaniel15@gmail.com>
2021-08-21 17:11:02 -0300

    build: Corrigido script de deploy no GitHub pages

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ npm run build &&
   git init && 
   git add . && 
   git commit -m "deploy" && 
-  git remote add origin https://github.com/pizzariacolosso/digital-menu.git && 
-  git push --force "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/pizzariacolosso/digital-menu.git" master && 
+  git remote add origin git@github.com:pizzariacolosso/digital-menu.git && 
+  git push -u --force origin main && 
   cd ../ && rm -rf digital-menu-build/
 
